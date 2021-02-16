8c77ae34bcdcf690e9ed94e3272cf2fb9ef1000d
Chithian(Hunter) <chithiansamony@gmail.com>
2021-02-16 08:38:37 +0700

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Chithian on 16/2/21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
